08c41cd4d5e9a84eee40571032d70bfcb1e13a52
Nandini Garg <a2023csd9570@imsec.ac.in>
2026-06-22 14:59:15 +0530

    Add files via upload

---
+++ 1/a1.js
@@ -0,0 +1,37 @@
+//Add 2 numbers
+
+let a = 15
+let b = 7
+let res = a + b
+console.log(res);
+
+//If conditions are obeyed or not
+
+console.log(a<10 && a>b);
+
+// Check the conditions
+console.log((a%10==0 || b%10==0) ? true : false );
+
+//First digit of a four digit number
+let p = 8976
+console.log(Math.floor (p/1000));
+
+//Last digit of a four digit number 
+console.log(p%10);
+
+
+// Remainder
+res = a%b
+console.log(res);
+
+//Multiply
+console.log(a * b);
+
+//Marks calculator
+let x = 80
+let y = 90
+let z = 95
+let total = x + y + z
+console.log(total);
+let average = total / 3
+console.log(average);
